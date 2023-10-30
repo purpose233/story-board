95ad83d38a8fb85e910c37a89aed7268e2b2decd
purpose <zhangsu.melon0@bytedance.com>
2023-10-30 16:41:46 +0800

    chore: add prettier config

--- src/components/config-panel/index.tsx
+++ src/components/config-panel/index.tsx
@@ -1,75 +1,52 @@
-import { Card, Input, InputNumber, Switch } from "@douyinfe/semi-ui";
-import { observer } from "mobx-react";
-import { useEditorStore, type EditorStore } from "../../store/element";
-import { elementVisualConfig } from "../../config/visual";
-import { IEditorElementConfig } from "../../typings";
+import { Card, Input, InputNumber, Switch } from '@douyinfe/semi-ui';
+import { observer } from 'mobx-react';
+import { useEditorStore, type EditorStore } from '../../store/element';
+import { elementVisualConfig } from '../../config/visual';
+import { IEditorElementConfig } from '../../typings';
 
-import "./index.css";
+import './index.css';
 
-const generateVisuals = (
-  elementConfig: IEditorElementConfig,
-  editorStore: EditorStore
-) => {
+const generateVisuals = (elementConfig: IEditorElementConfig, editorStore: EditorStore) => {
   const visualConfig = elementVisualConfig[elementConfig.type];
   if (visualConfig) {
-    return visualConfig.map((visual) => {
+    return visualConfig.map(visual => {
       const value = elementConfig.visuals[visual.channel] ?? visual.default;
       const onChange = (nextValue: any) => {
         if (editorStore.currentElement) {
           const element = editorStore.currentElement;
           const visuals = Object.assign({}, element.visuals, {
-            [visual.channel]: nextValue,
+            [visual.channel]: nextValue
           });
           const nextElement = Object.assign({}, element, { visuals });
           editorStore.updateElement(nextElement);
         }
       };
       switch (visual.type) {
-        case "string":
+        case 'string':
           return (
             <div className="config-panel-entry" key={visual.channel}>
-              <span className="config-panel-entry-label">
-                {visual.channel}:
-              </span>
-              <Input
-                defaultValue={value}
-                onChange={onChange}
-                style={{ width: 180 }}
-              ></Input>
+              <span className="config-panel-entry-label">{visual.channel}:</span>
+              <Input defaultValue={value} onChange={onChange} style={{ width: 180 }}></Input>
             </div>
           );
-        case "number":
+        case 'number':
           return (
             <div className="config-panel-entry" key={visual.channel}>
-              <span className="config-panel-entry-label">
-                {visual.channel}:
-              </span>
-              <InputNumber
-                defaultValue={value}
-                onChange={onChange}
-                style={{ width: 180 }}
-              />
+              <span className="config-panel-entry-label">{visual.channel}:</span>
+              <InputNumber defaultValue={value} onChange={onChange} style={{ width: 180 }} />
             </div>
           );
-        case "color":
+        case 'color':
           return (
             <div className="config-panel-entry" key={visual.channel}>
-              <span className="config-panel-entry-label">
-                {visual.channel}:
-              </span>
-              <Input
-                defaultValue={value}
-                onChange={onChange}
-                style={{ width: 180 }}
-              ></Input>
+              <span className="config-panel-entry-label">{visual.channel}:</span>
+              <Input defaultValue={value} onChange={onChange} style={{ width: 180 }}></Input>
             </div>
           );
-        case "boolean":
+        case 'boolean':
           return (
             <div className="config-panel-entry" key={visual.channel}>
-              <span className="config-panel-entry-label">
-                {visual.channel}:
-              </span>
+              <span className="config-panel-entry-label">{visual.channel}:</span>
               <Switch defaultChecked={value} onChange={onChange}></Switch>
             </div>
           );
@@ -84,10 +61,8 @@ export const ConfigPanel = observer(() => {
   const editorStore = useEditorStore();
 
   return (
-    <Card title="Config Panel" style={{ width: "100%", height: "100%" }}>
-      {editorStore.currentElement
-        ? generateVisuals(editorStore.currentElement, editorStore)
-        : null}
+    <Card title="Config Panel" style={{ width: '100%', height: '100%' }}>
+      {editorStore.currentElement ? generateVisuals(editorStore.currentElement, editorStore) : null}
     </Card>
   );
 });
